7ff575a19539bce68dc84e11b3fb4a027a12afda
PSS1998 <p_sadri_s@yahoo.com>
2020-05-28 02:34:32 +0430

    Update get-bourse-price.py

--- get-bourse-price.py
+++ get-bourse-price.py
@@ -1,10 +1,9 @@
+import sys
 
 
 
 text_file = open("list-bourse-csv.txt", "r")
 lines = text_file.read().split()
-# print lines
-# print len(lines)
 text_file.close()
 
 
@@ -22,22 +21,19 @@ def download(url, out_path):
     localName = url2name(url)
     req = urllib.request.Request(url)
     r = urllib.request.urlopen(req)
-    # if r.info().has_key('Content-Disposition'):
-    if 'Content-Disposition' in r.info():
-        # If the response has Content-Disposition, we take file name from it
-        localName = r.info()['Content-Disposition'].split('filename=')[1]
-        if localName[0] == '"' or localName[0] == "'":
-            localName = localName[1:-1]
-    elif r.url != url: 
-        # if we were redirected, the real file name we take from the final URL
-        localName = url2name(r.url)
 
+    localName = url.split("=")[-1]+".csv"
     localName = os.path.join(out_path, localName)
     f = open(localName, 'wb+')
     f.write(r.read())
     f.close()
 
-# fn = os.path.join(os.path.dirname(__file__), 'bourse-price')
 fn = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'bourse_price')
 for line in lines:
-	download(line, fn)
+    try:
+        download(line, fn)
+    except KeyboardInterrupt:
+        print("Bye")
+        sys.exit()
+    except:
+        print("Failed "+line)
